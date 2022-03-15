6449dbf5c376c4de6bde7fc7caf1a10a2baf6bcb
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-15 10:49:54 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name : "FlatIron",
+    address : "123 Fake Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] =  value;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    const emp = {...employee};
+
+    delete emp[key];
+
+    return emp;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+};
